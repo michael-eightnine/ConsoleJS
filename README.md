dc4bd94b821c90a5d6f48dd7a8a32457527508b5
Michael Smith <michael@eightnine.co>
2016-08-15 17:07:59 -0500

    Beta commit & README update
    Nothin else!

--- README.md
+++ README.md
@@ -1,2 +1,171 @@
 # consoleJS
-Test desc
+Vanilla JavaScript plugin for building an old-school console interface that runs custom defined commands and actions.
+
+Currently comes with 2 themes, and the ability to create your own via the source SCSS files or via the "unstyled" attribute.
+
+- No dependencies!
+- 1.9KB minified and Gzipped
+- Brought to you by [Michael Smith](http://www.eightnine.co/smith)
+
+## 1. Getting Started
+
+### 1.1. Installation
+
+The recommended way to get up and running with consoleJS is to load the consoleJS script before your closing `</body>` tag.
+
+```html
+<script src="path/to/script/console.min.js"></script>
+<script src="path/to/script/intializationScript.js"></script>
+```
+
+**Please note:** the script you use to initialize your console must be after the plugin script import.
+
+### 1.2. Basics
+
+When declaring your console, pass any configurations to apply them to the instance. It is highly recommended to add custom commands, even if you're using the default values for the other options.
+
+```js
+var myConsole = new Console(options);
+```
+
+After your console is declared, it's ready to be initialized via the 'build()' method/
+
+```js
+myConsole.build();
+```
+
+### 1.3. Quick Example
+
+A quick example of declaring and initializing a consoleJS instance might look like this:
+
+```js
+var myConsole = new Console({
+	introText: "My Example Console",
+	instructionText: "Type 'list' to see the all of the available commands",
+	commandHistory: false,
+	commands: [
+		{
+			command: "custom command",
+			action: "alert('command successful!')",
+			description: "'custom command' : generates an alert",
+			response: "hey user, you ran the command",
+			requires: null
+		}
+	]
+})
+
+myConsole.build()
+```
+
+Running the code above would generate a new consoleJS instance that accepts the command "custom command". When submitted it would generate the alert, and write the response "hey user, you ran the command" to the console.
+
+
+
+## 2. Configuration
+
+### 2.1. Default Options
+```js
+// Accepts the ID of a DOM element for the console to be created within, if 'null' appends console to the document's body
+consoleContainer: null,
+
+// Theme options: "ntsc", "windows95". Pass a different class name to build your own theme or pass "unstyled" to remove all default styling
+theme: "ntsc"
+
+// Accepts any string - this is the "header" text on the console window
+introText: "[ -- ConsoleJS -- ]",
+
+// Accepts any string - this is the text directly below introText
+instructionText: ":: Input 'list' for commands -- Append 'man' to a command for description ::",
+
+// Accepts any string - shorter strings are recommended
+consoleCaret: "// >",
+
+// Accepts any string - this text is shown to the user when they have entered an invalid command
+invalidCommandText: "command not recognized",
+
+// Accepts an integer - this is the amount of delay (in milliseconds) used when typing responses to the user
+typingInterval: 50,
+
+// Accepts a boolean - if true allows users to use the up and down arrow keys to select previously inputted commands
+commandHistory: true,
+
+//Accepts an integer - number of commands to save in the command history (only useful if commandHistory is true)
+commandHistoryCap: 5,
+
+//Accepts a boolean - set to false to disabled the default commands of "list", "minimize", and "maximize"
+enableDefaultCommands: true,
+
+//Accepts an array of command objects - this is where you place the actions and commands of your console
+commands: []
+
+```
+
+### 2.2. Building Custom Commands (The Command Object)
+
+The last option listed above is "commands" which is blank by default. This is where you can add in the commands that make your console usable.
+
+An example of a command declaration, with all of the available options, is as follows:
+
+```js
+commands: [
+	{
+		// Accepts a string - this is what the user will type in to access your command
+		command: "name of your command",
+
+		// Accepts a function - this is the action you wish to perform when the user runs the command
+		action: "exampleFunction()",
+
+		// Accepts a string - this will print when your use requests the man page for this command
+		description: "a description of what your command does and any other useful information",
+
+		// Accepts a string - this is what the console will respond to the user with - leave blank for no response
+		response: "hey, you ran the command"
+
+		// Accepts the name of another custom command - indicates the "requires" command must be run prior to running this command
+		requires: "previous command"
+	}
+]
+```
+
+## 3. Working with your Console
+
+### 3.1. Public Methods
+
+consoleJS offers a variety of public methods to help your customize your user's interaction with a custom console. These are listed below along with their arguments and function.
+
+These are all described using this context:
+```js
+var myConsole = new Console(options);
+```
+
+##### `myConsole.build()`
+
+Initializes your console object, this is required to run any other functions.
+
+##### `myConsole.getCommands()`
+
+Returns all of the commands currently available in the console.
+
+##### `myConsole.pushCommand(commandObject)`
+
+Adds a new command defined by a `commandObject`. (as seen in section 2.2.)
+
+##### `myConsole.removeCommand(commandName)`
+
+Removes a command whose name matches the string `commandName`.
+
+##### `myConsole.clearHistory(includeCommandHistory)`
+
+Clears the console history visible to the user. If `includeCommandHistory` is set to true it will also remove the previous commands avaiable via the arrow keys.
+
+##### `myConsole.pushMessage(message)`
+
+Prints the passed string to the console.
+
+##### `myConsole.minimize()`
+
+Minimizes the console window (assuming it isn't already)
+
+##### `myConsole.maximize()`
+
+Maximizes the console window (assuming it isn't already)
---
+++ src/js/console.js
@@ -0,0 +1,375 @@
+(function() {
+	//scoped globals for easy access in non init funcs
+	var commandList = null;
+	var optionsList = null;
+	var previousCommand = null;
+	var commandHistory = [];
+	var commandHistoryCap = 0;
+	var commandHistoryIndex = 0;
+	var defaultCommands = {
+		"list": [
+			"'list' : lists all commands available in the console"
+		],
+		"minimize": [
+			"'minimize' : shrinks the console window to the taskbar"
+		],
+		"maximize": [
+			"'maximize' : expands the console window to its max dimensions"
+		]
+	}
+
+	//*** CONSTRUCTOR ***
+	this.Terminal = function() {
+		this.terminal = null;
+		this.inputBox = null;
+		this.minimizeButton = null;
+
+		var defaults = {
+			terminalContainer: null,
+			theme: "ntsc",
+			introText: "[ -- Console.JS -- ]",
+			instructionText: ":: Input 'list' for commands -- Append 'man' to a command for description ::",
+			typingInterval: 50,
+			consoleCaret: "// >",
+			commandHistory: true,
+			commandHistoryCap: 5,
+			enableDefaultCommands: true,
+			invalidCommandText: "command not recognized",
+			commands: []
+		}
+
+		//check for user options
+		if (arguments[0] && typeof arguments[0] === "object") {
+			this.options = customSettings(defaults, arguments[0]);
+		}
+		else {
+			this.options = defaults;
+		}
+	}
+
+	//*** PRIVATE METHODS ***
+
+	//extend default options
+	function customSettings(source, properties) {
+		var property;
+		for (property in properties) {
+			if (properties.hasOwnProperty(property)) {
+				source[property] = properties[property];
+			}
+		}
+		return source;
+	}
+
+	//buiid modal
+	function createTerminal() {
+		var base = document.createDocumentFragment();
+		var introText,
+			instructionText,
+			inputContainer,
+			historyContainer,
+			historyScrollWrap;
+
+		//create terminal countainer and add theme class
+		this.terminal = document.createElement("div");
+		this.terminal.id = "consoleJsTerminal";
+		this.terminal.className = "console-js-terminal " + this.options.theme;
+
+		//create terminal layout/base
+		introText = document.createElement("p");
+			introText.className = "intro-text";
+			introText.innerHTML = this.options.introText;
+			this.minimizeButton = document.createElement("a");
+				this.minimizeButton.href = "#";
+				this.minimizeButton.className = "minimize-button";
+				this.minimizeButton.id = "minimizeButton";
+				introText.appendChild(this.minimizeButton);
+			this.terminal.appendChild(introText)
+
+		instructionText = document.createElement("p");
+			instructionText.className = "instruction-text";
+			instructionText.innerHTML = this.options.instructionText;
+			this.terminal.appendChild(instructionText);
+
+		historyScrollWrap = document.createElement("div");
+			historyScrollWrap.className = "terminal-history-scroll-wrap";
+			historyContainer= document.createElement("div");
+				historyContainer.id = "terminalHistoryContainer"
+				historyContainer.className = "terminal-history-container";
+				historyScrollWrap.appendChild(historyContainer);
+			this.terminal.appendChild(historyScrollWrap);
+
+		inputContainer = document.createElement("div");
+			inputContainer.className = "terminal-input-container";
+			inputContainer.innerHTML = "<label for='terminalInput' class='before'>" + this.options.consoleCaret + "</label>"
+				this.inputBox = document.createElement("input");
+				this.inputBox.type = "text";
+				this.inputBox.id = "terminalInput"
+				inputContainer.appendChild(this.inputBox);
+			this.terminal.appendChild(inputContainer);
+
+		base.appendChild(this.terminal);
+		if(this.options.terminalContainer != null) {
+			var insertInto = document.getElementById(this.options.terminalContainer);
+			insertInto.appendChild(base)
+		}
+		else {
+			document.body.appendChild(base);
+		}
+
+		//assign globals for easy access
+		commandList = this.options.commands;
+		optionsList = this.options;
+		previousCommand = "";
+		commandHistoryCap = this.options.commandHistoryCap;
+	}
+
+	//bind input for checking keycodes
+	function attachInput() {
+		this.inputBox.focus();
+		this.inputBox.addEventListener('keyup', function(e) {
+			var key = e.keyCode || e.which;
+
+			//on enter with non empty string
+			if ( key == 13 && this.value != "" ) {
+				var userCommand = this.value.toLowerCase().trim();
+				this.value = "";
+				checkCommand(userCommand)
+			}
+			//on up arrow roll through history
+			else if (key == 38) {
+				this.value = commandHistory[commandHistoryIndex];
+				if(commandHistoryIndex == 0) {
+					commandHistoryIndex = commandHistory.length - 1;
+				}
+				else {
+					commandHistoryIndex --;
+				}
+			}
+			//on down arrow roll through history (opposite direction from arrow up)
+			else if (key == 40) {
+				this.value = commandHistory[commandHistoryIndex];
+				if(commandHistoryIndex == commandHistory.length - 1) {
+					commandHistoryIndex = 0;
+				}
+				else {
+					commandHistoryIndex ++;
+				}
+			}
+		});
+	}
+
+	//bind minimize button
+	function attachMin() {
+		var terminal = document.getElementById("consoleJsTerminal");
+		this.minimizeButton.addEventListener('click', function(e) {
+			e.preventDefault();
+			if(terminal.classList.contains('minimize')) {
+				terminal.classList.remove('minimize');
+			}
+			else {
+				terminal.classList.add('minimize');
+			}
+		})
+	}
+
+	function checkCommand(userCommand) {
+		var manReg = new RegExp(".* man$");
+		var requestedMan = false;
+		var manCommand = null;
+
+		//is the user asking for the man page?
+		if(manReg.test(userCommand)) {
+			requestedMan = true;
+			manCommand = userCommand;
+			userCommand = userCommand.replace(" man", "");
+		}
+
+		//check if it matches default commands first
+		if((userCommand == "list" || userCommand == "minimize" || userCommand == "maximize") && optionsList.enableDefaultCommands) {
+			if(requestedMan) {
+				respond(defaultCommands[userCommand].toString(), manCommand);
+				return;
+			}
+			switch (userCommand) {
+				case "list":
+					listCommands();
+					break;
+				case "minimize":
+					Terminal.prototype.minimize.call(this);
+					respond(null, userCommand);
+					break;
+				case "maximize":
+					Terminal.prototype.maximize.call(this);
+					respond(null, userCommand);
+					break;
+			}
+			updateHistory(userCommand);
+			return;
+		}
+		//check against dev provided commands
+		for (var i = 0, len = commandList.length; i < len; i++) {
+			if( userCommand == commandList[i].command.toLowerCase() ) {
+				if (requestedMan) {
+					//return man page for command and skip any command init
+					respond(commandList[i].description, manCommand)
+					return;
+				}
+				//check requirement is met if command has requirement
+				if( commandList[i].requires != null && previousCommand != commandList[i].requires ) {
+					respond("another command is required first", userCommand);
+					previousCommand = userCommand;
+					updateHistory(userCommand);
+					return;
+				}
+				runCommand(commandList[i].action);
+				respond(commandList[i].response, userCommand);
+				previousCommand = userCommand;
+				updateHistory(userCommand);
+				return;
+			}
+			else if (i == len - 1) {
+				//rolled through all options, no match
+				respond(optionsList.invalidCommandText, userCommand);
+				updateHistory(userCommand);
+				return;
+			}
+		}
+	}
+
+	function runCommand(action) {
+		//run command in anon init wrapper
+		var commandAction = new Function(action);
+		commandAction();
+	}
+
+	function respond(responseText, commandText) {
+		var terminalHistoryContainer = document.getElementById("terminalHistoryContainer");
+
+		//print previous command if there was one
+		if (commandText != null) {
+			var oldCommand = document.createElement("p");
+				oldCommand.className = "previous-command"
+				oldCommand.innerHTML = optionsList.consoleCaret + " " + commandText;
+				terminalHistoryContainer.appendChild(oldCommand);
+		}
+
+		//print response if there is one
+		if (responseText != "" && responseText != null) {
+			var responseLine = document.createElement("p");
+				responseLine.className = "response";
+				terminalHistoryContainer.appendChild(responseLine);
+			updateScroll();
+
+			var currentCharIndex = 0;
+			//function for 'typing' effect
+			function typeLine() {
+				if( currentCharIndex >= responseText.length )
+					return;
+				var char = responseText[currentCharIndex];
+				responseLine.innerHTML = responseLine.innerHTML + char;
+				currentCharIndex ++;
+				setTimeout(typeLine, optionsList.typingInterval)
+			}
+			typeLine();
+		}
+		else {
+			updateScroll();
+		}
+	}
+
+	function updateHistory(userCommand) {
+		//limit command history to provided cap
+		if(commandHistory.length >= commandHistoryCap)
+			commandHistory.shift();
+
+		commandHistory.push(userCommand);
+		commandHistoryIndex = commandHistory.length - 1;
+	}
+
+	function updateScroll() {
+		//scroll down so most recent command/response is visible
+		setTimeout(function() {
+			var el = document.getElementById("terminalHistoryContainer");
+			el.scrollTop = el.scrollHeight;
+		}, 5); //timeout to prevent miscroll
+	}
+
+	function listCommands() {
+		var commandListText = "| ";
+		if (optionsList.enableDefaultCommands) {
+			for (var i = 0, len = Object.keys(defaultCommands).length; i < len; i++) {
+				commandListText = commandListText + Object.keys(defaultCommands)[i] + " | ";
+			}
+		}
+		for (var i = 0, len = commandList.length; i < len; i++) {
+			commandListText = commandListText + commandList[i].command + " | ";
+		}
+		respond.call(this, commandListText, "list");
+	}
+
+	//*** PUBLIC METHODS ***
+	Terminal.prototype.build = function() {
+		createTerminal.call(this);
+		attachInput.call(this);
+		attachMin.call(this);
+	}
+
+	Terminal.prototype.maximize = function() {
+		var terminal = document.getElementById("consoleJsTerminal");
+		if(terminal.classList.contains("minimize"))
+			terminal.classList.remove("minimize");
+	}
+
+	Terminal.prototype.minimize = function() {
+		var terminal = document.getElementById("consoleJsTerminal");
+		if(!terminal.classList.contains("minimize"))
+			terminal.classList.add("minimize");
+	}
+
+	Terminal.prototype.pushCommand = function(newCommand) {
+		if(typeof(newCommand) === "object") {
+			for (var i = 0, len = commandList.length; i < len; i++) {
+				if(newCommand.command.toLowerCase() == commandList[i].command.toLowerCase()) {
+					console.log("pushNewCommand failed: command " + newCommand.command + " already exists");
+					return;
+				}
+			}
+			commandList.push(newCommand)
+		}
+		else {
+			console.error("pushNewCommand failed: did not provide a valid command object");
+		}
+	}
+
+	Terminal.prototype.removeCommand = function(removeThis) {
+		for (var i = 0, len = commandList.length; i < len; i++) {
+			if(removeThis.toLowerCase() == commandList[i].command.toLowerCase()) {
+				var index = i;
+				commandList.splice(index, 1)
+				return;
+			}
+			else if (i == len - 1) {
+				console.error("removeCommand failed: command " + removeThis + " does not exists");
+				return;
+			}
+		}
+	}
+
+	Terminal.prototype.getCommands = function() {
+		return commandList;
+	}
+
+	Terminal.prototype.clearHistory = function(includeCommandHistory) {
+		var historyContainer = document.getElementById("terminalHistoryContainer");
+		while (historyContainer.firstChild) {
+			historyContainer.removeChild(historyContainer.firstChild);
+		}
+		if(includeCommandHistory)
+			commandHistory = [];
+	}
+
+	Terminal.prototype.pushMessage = function(message) {
+		respond(message, null);
+	}
+
+}());
